e3f20b8c966d4a5496e8d2677c6c0e2dc82670d7
Rajat Pratap <chauhanrajat515@gmail.com>
2025-07-28 20:02:16 +0530

    edited file.txt

--- Practice/script.js
+++ Practice/script.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 console.log("start");
 
-fs.writeFile("file.txt","Rajat",(()=>{}));
+fs.writeFile("file.txt","I will be a great coder one day",(()=>{}));
 fs.readFile("./file.txt","utf-8",((err,data)=>{
     if(err){
         console.log(err);
