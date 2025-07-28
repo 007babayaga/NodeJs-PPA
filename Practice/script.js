4cc4475749a57ec50d173c6825af4f85151c7d5d
Rajat Pratap <chauhanrajat515@gmail.com>
2025-07-28 20:33:11 +0530

    edited file.txt

--- Practice/script.js
+++ Practice/script.js
@@ -1,14 +1,21 @@
-const fs = require("fs");
+const fs = require("fs/promises");
 console.log("start");
 
-fs.writeFile("file.txt","I will be a great coder one day",(()=>{}));
-fs.readFile("./file.txt","utf-8",((err,data)=>{
-    if(err){
-        console.log(err);
-    }
-    else{
-        console.log(data);
-    }
-}))
+// fs.writeFile("file.txt","I will be a great coder one day",(()=>{}));
+// fs.readFile("./file.txt","utf-8",((err,data)=>{
+//     if(err){
+//         console.log(err);
+//     }
+//     else{
+//         console.log(data);
+//     }
+// }))
+
+const res = async ()=>{
+    await fs.writeFile("file.txt","I am ok",(()=>{}));
+    const ans = await fs.readFile("./file.txt","utf-8");
+    console.log(ans);
+}
+res();
 
 console.log("end");
