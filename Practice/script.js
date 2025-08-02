4f53e7a96fe8019dad5dc23f0231b1a5373a9fef
Rajat Pratap <chauhanrajat515@gmail.com>
2025-08-02 20:03:07 +0530

    practiced

--- Practice/script.js
+++ Practice/script.js
@@ -41,19 +41,43 @@
 // } catch (err) {
 //   console.error('crypto support is disabled!');
 // }
-const {pbkdf2,pbkdf2Sync} = require("crypto");
+// const {pbkdf2,pbkdf2Sync} = require("crypto");
 
 // const key = pbkdf2Sync('secret', 'salt', 1999900, 64, 'sha512');
 // console.log(key.toString('hex')); 
 // blocking the main thread
 
-for(let i=0;i<10;i++){
-    const start = performance.now();
-    const key = pbkdf2('secret', 'salt', 19999, 10, 'sha512',((err, hash)=>{
-    if (err) throw err;
-      console.log(hash.toString('hex')); 
-      const end = performance.now();
-      console.log("diff = " ,end-start);
-    }));
-    console.log("eee")
-}
+// for(let i=0;i<10;i++){
+//     const start = performance.now();
+//     const key = pbkdf2('secret', 'salt', 19999, 10, 'sha512',((err, hash)=>{
+//     if (err) throw err;
+//       console.log(hash.toString('hex')); 
+//       const end = performance.now();
+//       console.log("diff = " ,end-start);
+//     }));
+//     console.log("eee")
+// }
+// const http = require("http");
+
+// const server = http.createServer((req,res)=>{
+//   console.log("hello");
+//   res.end("Hello from Practice folder")
+// })
+// server.listen(3900,()=>{
+//   console.log("-------server is Running-----------");
+// })
+
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  const headers = new Headers({ 'Content-Type': 'text/html' });
+  res.setHeaders(headers);
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('ofk');
+}); 
+
+server.listen(3900,()=>{
+  console.log("----------server Started------------------");
+})
+
+
